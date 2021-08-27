03888aff27a7a74bc432a5d832e9cb506cd7683d
QiutianDog <1019652866@qq.com>
2021-08-27 21:42:37 +0800

    初始化

---
+++ src/main/java/fun/qiutiandog/mybatisdemo/MybatisDemoApplication.java
@@ -0,0 +1,20 @@
+package fun.qiutiandog.mybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * mybatis的练手项目
+ *
+ * @author QiutianDog
+ * @date 2021-08-27 21:28:33
+ * @version 1.0
+ */
+@SpringBootApplication
+public class MybatisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/qiutiandog/mybatisdemo/MybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.qiutiandog.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
